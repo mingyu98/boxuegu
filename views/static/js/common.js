77218451b105b5fadb662233bc1c59c08262828e
mmy <1092590270@qq.com>
2017-06-30 09:14:19 +0800

    部分模块化

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -1,8 +1,13 @@
+define(["jquery", "template", "cookie"], function($, template){
+	$(function(){
+		//判断当前页是不是在登录页面，如果是就不做如下的操作
+		if("/dashboard/login" != location.pathname){
+			//从cookie中获取登录成功后存储的用户信息
+	        var userInfo = JSON.parse($.cookie("userinfo"))
+	        var html = template("profile-tpl", userInfo);
 
-	NProgress.start();
-
-	NProgress.done();
-
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+	        //将模板渲染到页面中刚才挖坑的地方
+	        $("#userinfo").html(html);
+		}  
+    })
+})
